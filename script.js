b555464b923b2e2eeaea5c7dde8a9a570d4106ea
pnowakowski614 <p.nowakowski416@gmail.com>
2022-06-16 19:05:59 +0200

    fix(orbits): made sun immovable

--- script.js
+++ script.js
@@ -25,70 +25,74 @@ const ConstraintElementView = joint.dia.ElementView.extend({
        joint.dia.ElementView.prototype.pointermove.apply(this, [evt, intersection.x, intersection.y]);
    }
 });
-
-const graph = new joint.dia.Graph({}, { cellNamespace: namespace });
-
-const paper = new joint.dia.Paper({
-    el: document.getElementById('myholder'),
-    model: graph,
-    width: 1830,
-    height: 960,
-    gridSize: 1,
-    elementView: ConstraintElementView,
-    background: {
-        image: backgroundImage
-    },
-    cellViewNamespace: namespace
-});
-
-const createElement = (customPosition, resize, fillColor, labelText, constraintName) => {
-    const circle = new joint.shapes.standard.Circle({
-        position: customPosition,
-        size: {width: resize, height: resize},
-        attrs: {
-            body: {
-                fill: fillColor
-            },
-            label: {
-                text: labelText,
-                fill: 'black'
-            },
-        }
-    })
-    circle.prop('data/constraint', constraintName);
-    circle.addTo(graph);
-    return circle;
- }
  
-  
- const createOrbit = (constraint) => {
-    const orbit = V('<ellipse/>');
-  
-    orbit.attr({
-        cx: constraint.x,
-        cy: constraint.y,
-        rx: constraint.a,
-        ry: constraint.b,
-        fill: "transparent",
-        stroke: "black"
-    });
-      
-    V(paper.viewport).append(orbit);
-    return orbit;
- }
-
- const initPositions = (constraint, orbitPointX, orbitPointY, offsetX, offsetY) => {
+const graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+ 
+const paper = new joint.dia.Paper({
+   el: document.getElementById('myholder'),
+   model: graph,
+   width: 1830,
+   height: 960,
+   gridSize: 1,
+   elementView: ConstraintElementView,
+   interactive: function(elementView) {
+       if (elementView.model.attributes.data.status === false){
+           return {elementMove: false};
+       }
+       else return true;
+   },
+   background: {
+       image: backgroundImage
+   },
+   cellViewNamespace: namespace
+});
+ 
+const createElement = (customPosition, resize, fillColor, labelText, canMove, constraintName) => {
+   const circle = new joint.shapes.standard.Circle({
+       position: customPosition,
+       size: {width: resize, height: resize},
+       attrs: {
+           body: {
+               fill: fillColor
+           },
+           label: {
+               text: labelText,
+               fill: 'black'
+           },
+       }
+   })
+   circle.prop('data/status', canMove);
+   circle.prop('data/constraint', constraintName);
+   circle.addTo(graph);
+   return circle;
+}
+const createOrbit = (constraint) => {
+   const orbit = V('<ellipse/>');
+ 
+   orbit.attr({
+       cx: constraint.x,
+       cy: constraint.y,
+       rx: constraint.a,
+       ry: constraint.b,
+       fill: "transparent",
+       stroke: "black"
+   });
+     
+   V(paper.viewport).append(orbit);
+   return orbit;
+}
+ 
+const initPositions = (constraint, orbitPointX, orbitPointY, offsetX, offsetY) => {
     const position = constraint.intersectionWithLineFromCenterToPoint(g.point(orbitPointX, orbitPointY)).offset(offsetX, offsetY)
     return position;
- } 
-  
- const earthOrbit = createOrbit(earthConstraint);
- const mercuryOrbit = createOrbit(mercuryConstraint);
- const venusOrbit = createOrbit(venusConstraint);
- const marsOrbit = createOrbit(marsConstraint);
-
- const sun = createElement({x: 750, y: 380}, 200, 'yellow', 'Sun', earthConstraint);
- const mercury = createElement(initPositions(mercuryConstraint, 300, 300, -40, -10), 50, 'gray', 'Mercury', mercuryConstraint);
- const venus = createElement(initPositions(venusConstraint, 1500, 1500, -40, -50), 85, 'orange', 'Venus', venusConstraint);
- const earth = createElement(initPositions(earthConstraint, 100, 100, -40, -50), 100, 'blue', 'Earth', earthConstraint);
- const mars = createElement(initPositions(marsConstraint, 700, 1000, -40, -30), 70, 'red', 'Mars', marsConstraint);
+ }
+const earthOrbit = createOrbit(earthConstraint);
+const mercuryOrbit = createOrbit(mercuryConstraint);
+const venusOrbit = createOrbit(venusConstraint);
+const marsOrbit = createOrbit(marsConstraint);
+ 
+const sun = createElement({x: 750, y: 380}, 200, 'yellow', 'Sun', false, earthConstraint);
+const mercury = createElement(initPositions(mercuryConstraint, 300, 300, -40, -10), 50, 'gray', 'Mercury', true, mercuryConstraint);
+const venus = createElement(initPositions(venusConstraint, 1500, 1500, -40, -50), 85, 'orange', 'Venus', true, venusConstraint);
+const earth = createElement(initPositions(earthConstraint, 100, 100, -40, -50), 100, 'blue', 'Earth', true, earthConstraint);
+const mars = createElement(initPositions(marsConstraint, 700, 1000, -40, -30), 70, 'red', 'Mars', true, marsConstraint);
